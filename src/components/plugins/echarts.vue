27b2e93a0221fb7e8fdd2afb7a32d08f05a1e0d9
songyantao <songyantao@maxwell-gp.com.cn>
2025-08-29 11:26:21 +0800

    echarts-优化

--- src/components/plugins/echarts.vue
+++ src/components/plugins/echarts.vue
@@ -1,30 +1,49 @@
 <template>
-  <v-chart :option="option" style="height: 300px"></v-chart>
+  <p>消费总额:{{ total }}</p>
+  <v-chart :option="option" style="height: 300px" :loading="loading"></v-chart>
 </template>
   <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
-const option = ref({
-  title: { text: "2025 8月消费额", left: "center" },
-  tooltip: {},
-  legend:{show:true,left:10},
-  xAxis: { type: "category", data: ["花呗", "京东花呗", "美团", "微信"] },
-  yAxis: { type: "value" },
-  series: [
-    {
-      name:'消费',
-      type: "line",
-      show:true,
-      label:{
-        show:true,
-        position:'top'
-      },
-      data: [1745, 87.8, 118.24, 3500],
-    },
-  ],
-  grid:{
-    left:50,
-    right:50
-  }
+const arr = ref([1745, 87.8, 118.24, 3000, 1200]);
+const loading = ref(false);
+const option = ref({});
+const total = ref("");
+
+onMounted(() => {
+  options();
 });
+
+function options() {
+  loading.value = true;
+  total.value = arr.value.reduce((a, b) => a + b, 0);
+
+  option.value = {
+    title: { text: "2025 8月消费额", left: "center" },
+    tooltip: {},
+    legend: { show: true, left: 10 },
+    xAxis: {
+      type: "category",
+      data: ["花呗", "京东花呗", "美团", "微信", "拼多多"],
+    },
+    yAxis: { type: "value" },
+    series: [
+      {
+        name: "消费",
+        type: "line",
+        show: true,
+        label: {
+          show: true,
+          position: "top",
+        },
+        data: arr.value,
+      },
+    ],
+    grid: {
+      left: 50,
+      right: 50,
+    },
+  };
+  loading.value = false;
+}
 </script>
